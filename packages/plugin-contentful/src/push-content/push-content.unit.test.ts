03d4f553002153e46375c363f757aeb9a13ee654
Jacco Meijer <jacco@jaccomeijer.nl>
2021-09-03 20:23:10 +0200

    chore: added push content test

--- packages/plugin-contentful/src/push-content/push-content.unit.test.ts
+++ packages/plugin-contentful/src/push-content/push-content.unit.test.ts
@@ -1,12 +1,27 @@
+import chalk from 'chalk'
 import { environmentMock } from '../lib/contentful-mock'
 import { pushContent } from './push-content'
 import { topicAction } from './__fixtures__/topic-action'
 
 describe('Push content should', () => {
-  // const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
-  // beforeEach(() => {
-  //   consoleSpy.mockReset()
-  // })
+  const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+  beforeEach(() => {
+    consoleSpy.mockReset()
+  })
+
+  const headingResponse = [[chalk('- default locale: en-US')]]
+
+  const contentfulActionResponse = [
+    [chalk.bold.underline('\nContentfulAction')],
+    [chalk.underline('myActions')],
+    [chalk('- pushing entry testAction with 1 fields (id:myContentActionId)')],
+  ]
+
+  const contentfulTopicResponse = [
+    [chalk.bold.underline('\nContentfulTopic')],
+    [chalk.underline('myTopics')],
+    [chalk('- pushing entry testTopic with 2 fields')],
+  ]
 
   test('process topicAction correctly', async () => {
     await pushContent({
@@ -14,5 +29,10 @@ describe('Push content should', () => {
       options: { force: true },
       typeData: topicAction,
     })
+    expect(consoleSpy.mock.calls).toEqual([
+      ...headingResponse,
+      ...contentfulActionResponse,
+      ...contentfulTopicResponse,
+    ])
   })
 })
