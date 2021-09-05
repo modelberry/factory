1733aeb423370f7b49f44f2d8bfcd10937ee765c
Jacco Meijer <jacco@jaccomeijer.nl>
2021-09-05 18:10:47 +0200

    chore: draft setup getTsSyntaxKind

--- packages/plugin-contentful/src/pull-models/get-ts-syntax-kind.ts
+++ packages/plugin-contentful/src/pull-models/get-ts-syntax-kind.ts
@@ -6,7 +6,61 @@ export interface GetTsSyntaxKind {
 }
 
 export const getTsSyntaxKind = ({ contentField }: GetTsSyntaxKind) => {
-  if (contentField.type === 'Symbol') return tsSyntaxKind.StringKeyword
+  switch (contentField.type) {
+    case 'Symbol':
+      return tsSyntaxKind.StringKeyword
 
+    case 'Text':
+      return tsSyntaxKind.StringKeyword
+
+    case 'RichText':
+      // text?: {
+      //   json?: Document
+      //   links: {
+      //     assets: {
+      //       block: ContentfulAsset[]
+      //       hyperlink: ContentfulAsset[]
+      //     }
+      //     entries: {
+      //       block: ContentfulTextEntry[]
+      //       hyperlink: ContentfulTextEntry[]
+      //       inline: ContentfulTextEntry[]
+      //     }
+      //   }
+      // }
+
+      break
+
+    case 'Integer':
+      return tsSyntaxKind.NumberKeyword
+
+    case 'Number':
+      return tsSyntaxKind.NumberKeyword
+
+    case 'Date':
+      return tsSyntaxKind.StringKeyword
+
+    case 'Boolean':
+      return tsSyntaxKind.BooleanKeyword
+
+    case 'Location':
+      return tsSyntaxKind.StringKeyword
+
+    case 'Object':
+      return tsSyntaxKind.StringKeyword
+
+    case 'Link':
+      // linkType: Asset, Entry
+      break
+    case 'Array':
+      // {type: 'Array', items: {type: 'Symbol'}}
+      // {type: 'Array', items: {type: 'Link', linkType: 'Entry'}}
+      // {type: 'Array', items: {type: 'Link', linkType: 'Asset'}}
+
+      break
+
+    default:
+      return tsSyntaxKind.StringKeyword
+  }
   return tsSyntaxKind.StringKeyword
 }
