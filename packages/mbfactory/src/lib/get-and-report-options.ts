277cc77a3e87476f75ecd609076f1fa5df757c93
Jacco Meijer <jacco@jaccomeijer.nl>
2021-10-02 23:06:53 +0200

    chore: change option color from red to blue

--- packages/mbfactory/src/lib/get-and-report-options.ts
+++ packages/mbfactory/src/lib/get-and-report-options.ts
@@ -17,18 +17,22 @@ export const getAndGeportOptions = ({ argv }: GetAndGeportOptions) => {
   }
 
   if (options.dryRun) {
-    console.log(chalk(`- dry run enabled, running without making any changes`))
+    console.log(
+      chalk.blue(`- dry run enabled, running without making any changes`)
+    )
   }
   if (options.force) {
-    console.log(chalk(`- force enabled, ignoring all messages and warnings`))
+    console.log(
+      chalk.blue(`- force enabled, ignoring all messages and warnings`)
+    )
   }
   if (options.locale) {
     console.log(
-      chalk.red(`- overriding @modelberry {@locale} with ${options.locale}`)
+      chalk.blue(`- overriding @modelberry {@locale} with ${options.locale}`)
     )
   }
   if (options.type) {
-    console.log(chalk.red(`- filtering by type value ${options.type}`))
+    console.log(chalk.blue(`- filtering by type value ${options.type}`))
   }
 
   return options
