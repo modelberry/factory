93d90eff18a1dbf1a52b9e6afb98b8245827dad7
Jacco Meijer <jacco@jaccomeijer.nl>
2021-10-03 14:47:37 +0200

    chore: use type instead of interface

--- packages/plugin-contentful/src/pull-models/create-contentful-reference-type-declaration.ts
+++ packages/plugin-contentful/src/pull-models/create-contentful-reference-type-declaration.ts
@@ -3,7 +3,7 @@ import ts from 'typescript'
 export const createContentfulReferenceTypeDeclaration = () => {
   const file = ts.createSourceFile(
     'dummy.ts',
-    `export interface ContentfulReference { sys: { id: string } }`,
+    `export type ContentfulReference { sys: { id: string } }`,
     ts.ScriptTarget.Latest,
     false,
     ts.ScriptKind.TS
