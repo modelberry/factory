752beaa9fd586896f2792b211240bf53c4652760
Jacco Meijer <jacco@jaccomeijer.nl>
2022-07-06 21:14:12 +0200

    chore: add test for removeLinkAndLinkType

---
+++ packages/plugin-contentful/src/lib/remove-link-and-link-type.unit.test.ts
@@ -0,0 +1,42 @@
+import { removeLinkAndLinkType } from './remove-link-and-link-type'
+
+describe('removeLinkAndLinkType should', () => {
+  test('remove link and linkType from items', async () => {
+    const fieldValue = {
+      id: 'footer',
+      name: 'Footer',
+      type: 'Array',
+      localized: false,
+      required: false,
+      validations: [],
+      disabled: false,
+      omitted: false,
+      items: {
+        type: 'Link',
+        validations: [{ linkContentType: ['navigationSegment'] }],
+        linkType: 'Entry',
+      },
+    }
+    const remoteEntryFieldValue = removeLinkAndLinkType({
+      fieldValue,
+    })
+    expect(remoteEntryFieldValue).toMatchSnapshot()
+  })
+  test('remove link and linkType', async () => {
+    const fieldValue = {
+      id: 'social',
+      name: 'Social',
+      type: 'Link',
+      localized: false,
+      required: false,
+      validations: [{ linkContentType: ['navigationSegment'] }],
+      disabled: false,
+      omitted: false,
+      linkType: 'Entry',
+    }
+    const remoteEntryFieldValue = removeLinkAndLinkType({
+      fieldValue,
+    })
+    expect(remoteEntryFieldValue).toMatchSnapshot()
+  })
+})
