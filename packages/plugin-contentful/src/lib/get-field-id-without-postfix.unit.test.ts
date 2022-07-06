1a7654d036a2741565209484f55d359852423992
Jacco Meijer <jacco@jaccomeijer.nl>
2022-07-06 20:14:08 +0200

    chore: add test for getFieldIdWithoutPostfix

---
+++ packages/plugin-contentful/src/lib/get-field-id-without-postfix.unit.test.ts
@@ -0,0 +1,16 @@
+import { getFieldIdWithoutPostfix } from './get-field-id-without-postfix'
+
+describe('getFieldIdWithoutPostfix should', () => {
+  test('get fieldId without postfix when no postfix is present', async () => {
+    const fieldIdWithoutPostfix = getFieldIdWithoutPostfix({
+      fieldId: 'testField',
+    })
+    expect(fieldIdWithoutPostfix).toEqual('testField')
+  })
+  test('get fieldId without postfix when postfix is present', async () => {
+    const fieldIdWithoutPostfix = getFieldIdWithoutPostfix({
+      fieldId: 'someFieldCollection',
+    })
+    expect(fieldIdWithoutPostfix).toEqual('someField')
+  })
+})
